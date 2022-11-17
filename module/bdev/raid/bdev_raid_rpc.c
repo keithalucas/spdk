0f11a805ca2005b8fb1421183be03c30927026a2
Krzysztof Smolinski <krzysztof.smolinski@intel.com>
2022-11-17 11:40:49 +0100

    module/raid: data offset and data size implementation
    When raid bdev is created with superblock parameter then all data on
    this bdev should be shifted by some offset. Such space at the beginning
    of bdev will be used to store on-disk raid metadata.
    
    Signed-off-by: Krzysztof Smolinski <krzysztof.smolinski@intel.com>
    Signed-off-by: Artur Paszkiewicz <artur.paszkiewicz@intel.com>
    Change-Id: I2545a2b00a651ef5332ca1757da0110a63914a43

--- module/bdev/raid/bdev_raid_rpc.c
+++ module/bdev/raid/bdev_raid_rpc.c
@@ -225,7 +225,7 @@ rpc_bdev_raid_create(struct spdk_jsonrpc_request *request,
 	}
 
 	rc = raid_bdev_create(req.name, req.strip_size_kb, req.base_bdevs.num_base_bdevs,
-			      req.level, &raid_bdev);
+			      req.level, req.superblock, &raid_bdev);
 	if (rc != 0) {
 		spdk_jsonrpc_send_error_response_fmt(request, rc,
 						     "Failed to create RAID bdev %s: %s",
