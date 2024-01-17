1810c0c0fb75e5b455448be03a118d88f1517f74
Damiano Cipriani <damiano.cipriani@suse.com>
2024-01-17 17:35:40 +0100

    vbdev_lvol: add xattr to vbdev_lvol_create_snapshot
    New params have been added to vbdev_lvol_create_snapshot to
    set xattrs in the newly created snapshot.
    
    Signed-off-by: Damiano Cipriani <damiano.cipriani@suse.com>

--- test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut.c
+++ test/unit/lib/bdev/vbdev_lvol.c/vbdev_lvol_ut.c
@@ -39,6 +39,7 @@ bool g_bdev_alias_already_exists = false;
 bool g_lvs_with_name_already_exists = false;
 bool g_ext_api_called;
 bool g_bdev_is_missing = false;
+bool g_snapshot_xattr_called = false;
 
 DEFINE_STUB_V(spdk_bdev_module_fini_start_done, (void));
 DEFINE_STUB(spdk_bdev_get_memory_domains, int, (struct spdk_bdev *bdev,
@@ -887,6 +888,8 @@ spdk_lvol_create_snapshot_with_xattrs(struct spdk_lvol *lvol, const char *snapsh
 				      const char *const *xattrs, size_t xattrs_num,
 				      spdk_lvol_op_with_handle_complete cb_fn, void *cb_arg)
 {
+	g_snapshot_xattr_called = true;
+	spdk_lvol_create_snapshot(lvol, snapshot_name, cb_fn, cb_arg);
 }
 
 void
@@ -1066,6 +1069,7 @@ ut_lvol_snapshot(void)
 	int sz = 10;
 	int rc;
 	struct spdk_lvol *lvol = NULL;
+	char *xattrs[] = {"par", "val"};
 
 	/* Lvol store is successfully created */
 	rc = vbdev_lvs_create("bdev", "lvs", 0, LVS_CLEAR_WITH_UNMAP, 0,
@@ -1087,10 +1091,31 @@ ut_lvol_snapshot(void)
 	lvol = g_lvol;
 
 	/* Successful snap create */
-	vbdev_lvol_create_snapshot(lvol, "snap", vbdev_lvol_create_complete, NULL);
+	vbdev_lvol_create_snapshot(lvol, "snap", NULL, 0, vbdev_lvol_create_complete, NULL);
 	SPDK_CU_ASSERT_FATAL(rc == 0);
 	CU_ASSERT(g_lvol != NULL);
 	CU_ASSERT(g_lvolerrno == 0);
+	CU_ASSERT(g_snapshot_xattr_called == false);
+
+	/* Snap create with NULL xattrs and xattrs number > 0 */
+	g_lvol = NULL;
+	vbdev_lvol_create_snapshot(lvol, "snap2", NULL, 2,
+				   vbdev_lvol_create_complete, NULL);
+	CU_ASSERT(g_lvol == NULL);
+	CU_ASSERT(g_lvolerrno == -EINVAL);
+
+	/* Snap create with valid xattrs and 0 xattrs number */
+	vbdev_lvol_create_snapshot(lvol, "snap2", (const char *const *)&xattrs, 0,
+				   vbdev_lvol_create_complete, NULL);
+	CU_ASSERT(g_lvol == NULL);
+	CU_ASSERT(g_lvolerrno == -EINVAL);
+
+	/* Successful snap create with xattr */
+	vbdev_lvol_create_snapshot(lvol, "snap4", (const char *const *)&xattrs, 2,
+				   vbdev_lvol_create_complete, NULL);
+	CU_ASSERT(g_lvol != NULL);
+	CU_ASSERT(g_lvolerrno == 0);
+	CU_ASSERT(g_snapshot_xattr_called == true);
 
 	/* Successful lvol destroy */
 	vbdev_lvol_destroy(g_lvol, lvol_store_op_complete, NULL);
@@ -1137,7 +1162,7 @@ ut_lvol_clone(void)
 	lvol = g_lvol;
 
 	/* Successful snap create */
-	vbdev_lvol_create_snapshot(lvol, "snap", vbdev_lvol_create_complete, NULL);
+	vbdev_lvol_create_snapshot(lvol, "snap", NULL, 0, vbdev_lvol_create_complete, NULL);
 	SPDK_CU_ASSERT_FATAL(rc == 0);
 	SPDK_CU_ASSERT_FATAL(g_lvol != NULL);
 	CU_ASSERT(g_lvolerrno == 0);
